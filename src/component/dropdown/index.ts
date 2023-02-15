5d8f55b51f596d6e9cd03226a1c7164c631b43eb
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2023-02-15 17:18:25 +0900

    reafactor : View 관심사 분리

--- src/component/dropdown/index.ts
+++ src/component/dropdown/index.ts
@@ -1,13 +1,17 @@
 import { ActionDropdown, sortReducer } from "../../redux/reducer";
 import { createAction, createStore } from "../../redux/reduxLike";
+import { View } from "./View";
 
 export const storeDropdown = createStore(sortReducer);
-const qtySelector = [5, 15];
-const sortByBig = (dataSortValue) =>
+
+interface ICurrentQty {
+  count?: number;
+}
+const sortByBig = (dataSortValue: number) =>
   storeDropdown.dispatch(
     createAction(ActionDropdown.SORTBYBIGLIST, { sortData: dataSortValue })
   );
-const sortBySmall = (dataSortValue) =>
+const sortBySmall = (dataSortValue: number) =>
   storeDropdown.dispatch(
     createAction(ActionDropdown.SORTBYSMALLLIST, {
       sortData: dataSortValue,
@@ -15,41 +19,24 @@ const sortBySmall = (dataSortValue) =>
   );
 
 export const dropdown = () => {
-  const dropdown = document.querySelector("#dropdown");
-  document.querySelector("#dropdown").addEventListener("click", (e: Event) => {
+  const dropdown: HTMLDivElement = document.querySelector("#dropdown");
+  const currentQty: ICurrentQty = storeDropdown.getState();
+
+  dropdown.addEventListener("click", (e: Event) => {
     const target = e.target as HTMLLIElement;
     const dropdownArea = target.closest("#dropdown");
-    const dataSortValue = target.getAttribute("data-sort");
+    const dataSortValue = Number(target.getAttribute("data-sort"));
 
     if (dropdownArea.classList.contains("clicked") && dataSortValue) {
-      dataSortValue === "5"
+      dataSortValue === 5
         ? sortBySmall(dataSortValue)
         : sortByBig(dataSortValue);
-      dropdown.classList.remove("clicked");
+      dropdown.classList.remove("clicked"); //Todo 하드코딩
     } else {
       dropdown.classList.add("clicked");
     }
-    dropdownRender();
+    return View(currentQty);
   });
 };
 
-const dropdownRender = () => {
-  const dropdown = document.querySelector("#dropdown");
-  const currentQty = storeDropdown.getState();
-  if (dropdown.classList.contains("clicked")) {
-    document.querySelector("#dropdown").innerHTML = `
-    <ul class="dropdown-menu" id="qty-selector">
-     ${qtySelector
-       .map((qty) => `<li data-sort="${qty}">${qty}개</li>`)
-       .join("")}
-    </ul>
-    </div>
-    `;
-  } else {
-    document.querySelector(
-      "#dropdown"
-    ).innerHTML = `<ul class="dropdown-menu">${currentQty.count}개씩</ul>`;
-  }
-};
-
-storeDropdown.subscribe(dropdownRender);
+storeDropdown.subscribe(View);
